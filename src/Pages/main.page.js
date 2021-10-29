89e6c5bf853e4d35a7780734c4ea162cd031e0d3
keivanmojmali <mojmalik@gmail.com>
2021-10-28 17:58:56 -0700

    adding in the vars when stored and parsed

--- src/Pages/main.page.js
+++ src/Pages/main.page.js
@@ -5,34 +5,67 @@ export default class MainPage extends React.Component {
         super(props);
         this.state = {
             available: undefined,
-            missing: undefined
+            missing: undefined,
+            json: undefined,
+            waiting: 'no'
         }
         this.returnJsonArea = this.returnJsonArea.bind(this);
     }
-    storeJson(){
-        
+    onChange(text) {
+        //This function will take the text area text and store it in state
+        const json = text.target.value;
+        this.setState({ json });
     }
-    returnJsonArea(){
-        return(
-            <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    parseAndStore(){
+        //this function will parse and store the json that was entered into the field
+        this.setState({waiting: 'yes'});
+        const parsedJSON = JSON.parse(this.state.json);
+        if(!parsedJSON.available){
+            this.setState({waiting:'no'})
+            return Window.alert('Please enter valid JSON file')
+        }
+        if(!parsedJSON.missing){
+            this.setState({waiting:'no'})
+            return Window.alert('Please enter valid JSON file')
+        }
+        const available = parsedJSON.available;
+        const missing = parsedJSON.missing;
+
+        this.setState({available,missing,waiting:'no'});
+      
+    }
+    returnJsonArea() {
+        //This function will return the initial text area plus button that will be displayed
+        return (
+            <div className='col w-50 h-50 m-auto bg-light'>
+                <form>
+                    <div class="form-group">
+                        <label for="json">Please Enter Json Below</label>
+                        <textarea class="form-control" id="json" rows="10" onChange={this.onChange}></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary" >Submit</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
         )
     }
-    displayLogic(){
-        if(this.state.available === undefined || this.state.missing === undefined){
-            return this.returnJsonArea;
-        } else{
-            
+    displayLogic() {
+        //this function will decide what to render based on what is stored in state
+
+        if(this.state.waiting === 'yes') {
+            return <h1>...parsing</h1>
+        }
+
+        if (this.state.available === undefined || this.state.missing === undefined) {
+            return this.returnJsonArea();
+        } else {
+            <div>
+                <h1>Available: {this.state.available}</h1>
+                <h1>Missing: {this.state.missing}</h1>
+            </div>
         }
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className='container-fluid bg-dark d-flex justify-content-center align-items-center h-100'>
                 <div className='bg-dark row w-100 h-100'>
                     {this.displayLogic()}
