91b6a563a9a2cd86f1550db565667d5a962faa14
keivanmojmali <mojmalik@gmail.com>
2021-10-28 17:14:09 -0700

    mainpage set up

---
+++ src/Pages/main.page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            available: '',
+            missing: ''
+        }
+    }
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
